6981ea6f5c90fc338f8f8d1bbf6589b9ded378ab
nhuanthi <tlam1288@gmail.com>
2020-08-18 16:34:53 -0700

    fixed travis error and moved bootstrap import into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
@@ -11,7 +12,9 @@ import registerServiceWorker from "./registerServiceWorker";
 if (localStorage.getItem("id_token")) {
   // then we will attach it to the headers of each request from react
   // application via axios
-  axios.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem("id_token")}`;
+  axios.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem(
+    "id_token"
+  )}`;
 }
 
 ReactDOM.render(
