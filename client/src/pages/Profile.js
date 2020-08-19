089a2b6af91fd928312167f4c4b5dcbc4ae4fda0
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-08-19 10:40:43 -0700

    created imput card component and displays on profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,6 +4,7 @@ import insightAPI from "./../utils/insightAPI";
 import Container from "../components/Container";
 import { Link } from "react-router-dom";
 import { useAuth } from "../utils/auth";
+import Task from "../components/Form/taskCard"
 
 function Profile() {
   const [username, setUsername] = useState("");
@@ -67,6 +68,7 @@ function Profile() {
           <p>Low Temp:</p>
         </div>
       </div>
+      <Task />
       <Link to="/">Go home</Link>
     </Container>
   );
