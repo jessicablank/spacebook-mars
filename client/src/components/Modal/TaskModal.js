be02b9483039489bf61b42a23f5f7c5262004532
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-09-11 20:22:09 -0700

    added form inputs to task detail modal

--- client/src/components/Modal/TaskModal.js
+++ client/src/components/Modal/TaskModal.js
@@ -1,30 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import taskAPI from "../../utils/taskAPI";
+import "./style.css";
 
-
-function TaskModal({ onHide, task }) {
-  const [tasksData, setTasksData] = useState([]);
+// function TaskModal({ onHide, task }) {
+//   const [tasksData, setTasksData] = useState([]);
 
 function updateTask(id) {
-  console.log("button works")
-
+  console.log("button works");
 }
 
-
-
 //Used on the Tasks Page
-function TaskModal() {
+function TaskModal({ task, onHide }) {
   return (
     <>
       <Modal show={true} onHide={onHide}>
         <Modal.Header closeButton>
-          <Modal.Title><input value={task.title} onChange={(e) => task.titleHandler(e)}/></Modal.Title>
+          <Modal.Title>
+            <Form>
+              <Form.Group controlId="TaskTitleText">
+                <Form.Control className = "TaskTitleText" as="textarea" size="lg" rows="1" value={task.title} />
+              </Form.Group>
+            </Form>
+          </Modal.Title>
         </Modal.Header>
-        <Modal.Body><input className = "module-body" value={task.textBody}/></Modal.Body>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="TaskBodyText">
+              <Form.Control
+                as="textarea"
+                multiple
+                value={task.textBody}
+              ></Form.Control>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
         <Modal.Footer>
-        <Button variant="primary" onClick={updateTask}>
+          <Button variant="primary" onClick={updateTask}>
             Save Changes
           </Button>
           <Button variant="secondary" onClick={onHide}>
@@ -34,7 +48,6 @@ function TaskModal() {
       </Modal>
     </>
   );
-}
 }
 
 export default TaskModal;
